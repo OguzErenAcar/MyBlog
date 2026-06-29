07e2fd6c74a683c0b389cb14cb6b8245d779c194
oguz <oguz@oguz-MacBook-Pro.local>
2026-06-29 23:25:48 +0300

    auto: 2026-06-29 23:25

--- src/Redux/Slices/projectListSlice.js
+++ src/Redux/Slices/projectListSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, current } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { Octokit } from "octokit";
 
 const initialState = { 
