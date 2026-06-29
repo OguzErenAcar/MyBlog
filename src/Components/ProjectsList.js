b4f47893bcd8141b95326e53f8157f860031b4d4
oguz <oguz@oguz-MacBook-Pro.local>
2026-06-29 22:58:34 +0300

    auto: 2026-06-29 22:58

--- src/Components/ProjectsList.js
+++ src/Components/ProjectsList.js
@@ -1,45 +1,70 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
-import React, { Component } from "react";
-import {useMemo ,memo } from 'react';
+import React from "react";
 
 import {
   getProjectsList,
   setSelected,
-  clear,
-  fetchDetails 
+  fetchDetails
 } from "../Redux/Slices/projectListSlice";
 
+const ProjectImgWithSkeleton = ({ src, alt }) => {
+  const [loaded, setLoaded] = useState(false);
+  return (
+    <>
+      {!loaded && <div className="skeleton skeleton-project-img"></div>}
+      <img
+        alt={alt}
+        src={src}
+        className="projectImg"
+        onLoad={() => setLoaded(true)}
+        style={loaded ? {} : { display: 'none' }}
+      />
+    </>
+  );
+};
+
+const SkeletonGrid = ({ count }) => (
+  <>
+    {Array.from({ length: count }).map((_, i) => (
+      <div key={i} className="skeleton-project-card">
+        <div className="skeleton skeleton-project-img"></div>
+        <div className="skeleton skeleton-text skeleton-project-name"></div>
+      </div>
+    ))}
+  </>
+);
+
 const ProjectsList = () => {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
   const [todo, settodo] = useState([]);
   const [list, setlist] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {  
-    dispatch(getProjectsList()).then((res) => { 
+  useEffect(() => {
+    dispatch(getProjectsList()).then((res) => {
       const array = res.payload.map(item=>{return {name:item.name}})
       settodo(array)
     });
   }, []);
-  
-  useEffect(()=>{ 
-    var updatelist=[]
-    todo.forEach((element,i) => { 
-      dispatch(fetchDetails(element.name)).then((item,i)=>{ 
+
+  useEffect(()=>{
+    if (todo.length === 0) return;
+    let completed = 0;
+    todo.forEach((element) => {
+      dispatch(fetchDetails(element.name)).then((item)=>{
         const image=getImg(item.payload.Readme)
-       const obj={
+        const obj={
           name:item.payload.name,
           Readme:item.payload.Readme,
           image
         }
         setlist(prevlist=>prevlist.concat(obj))
+        completed++;
+        if (completed === todo.length) setLoading(false);
       });
-    }); 
+    });
   },[todo])
-
-useEffect(()=>{
-  console.log(list)
-},[list]) 
 
   const getImg = (Readme) => {
     try {
@@ -57,20 +82,20 @@ useEffect(()=>{
 
   const selectProject = (id) => {
     dispatch(setSelected(list.filter((item) => item.id === id)));
-    console.log("item selected.");
   };
 
   return (
-    <div class="projectsGrid">
+    <div className="projectsGrid">
+      {loading && <SkeletonGrid count={6} />}
       {list.map((project, index) => (
         <div
+          key={project.name}
           onClick={() => selectProject(project.id)}
-          id={"projeItem" + { index }}
+          className="skeleton-project-card"
         >
-          <img
-            alt={project.name}
+          <ProjectImgWithSkeleton
             src={project.image || "/images/defaultproject.jpg"}
-            className="projectImg"
+            alt={project.name}
           />
           <a href={"Projects/" + project.name}>{project.name}</a>
         </div>
