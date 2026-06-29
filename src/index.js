ff58c5d96e6a5957ac8916484deb83e7e12e8d6a
oguz <oguz@oguz-MacBook-Pro.local>
2026-06-29 23:25:50 +0300

    auto: 2026-06-29 23:25

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import store from "./Redux/store"
 import {Provider} from 'react-redux'
 import "./CSS/styles.css"   
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
