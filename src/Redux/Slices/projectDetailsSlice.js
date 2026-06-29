7fe309695e81d3f409f3be3db37748748076cfc8
oguz <oguz@oguz-MacBook-Pro.local>
2026-06-29 23:07:45 +0300

    auto: 2026-06-29 23:07

--- src/Redux/Slices/projectDetailsSlice.js
+++ src/Redux/Slices/projectDetailsSlice.js
@@ -2,7 +2,8 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { Octokit } from "octokit";
 
 const initialState = {
-  data: "",
+  readme: null,
+  repo: null,
   projectName: null,
   loading: false,
   error: null,
@@ -12,49 +13,58 @@ const octokit = new Octokit({
   auth: process.env.REACT_APP_GITHUB_TOKEN,
 });
 
-const fetchReadme = async (projectName) => {
-  const result = await octokit
-    .request("GET /repos/{owner}/{repo}/contents/{path}", {
-      owner: "OguzErenAcar",
-      repo: projectName,
-      path: "README.md",
-      headers: {
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-    })
-    .then(async (response) => {
-      const url = response.data.download_url;
-      const data = await fetch(url).then((res) => res.text()); 
-      return {data};
-    });
-  return result;
-};
-
-//createAsyncThunk bir plain obj döndürür o yüzden func içine alma
-//fetchDetails(parametre) girince çalışıyor :O
 export const fetchDetails = createAsyncThunk(
   "projectDetails/getDetails",
-  async (_, thunkAPI) => {
-    const projectName = thunkAPI.getState().projectDetails.projectName;
-    const result = await fetchReadme(projectName); 
-    return result;  
+  async (projectName) => {
+    const repoRes = await octokit.request("GET /repos/{owner}/{repo}", {
+      owner: "OguzErenAcar",
+      repo: projectName,
+      headers: { "X-GitHub-Api-Version": "2022-11-28" },
+    });
+
+    let readme = null;
+    try {
+      const readmeRes = await octokit.request(
+        "GET /repos/{owner}/{repo}/contents/{path}",
+        {
+          owner: "OguzErenAcar",
+          repo: projectName,
+          path: "README.md",
+          headers: { "X-GitHub-Api-Version": "2022-11-28" },
+        }
+      );
+      const url = readmeRes.data.download_url;
+      readme = await fetch(url).then((res) => res.text());
+    } catch (e) {
+      readme = null;
+    }
+
+    const repo = repoRes.data;
+    return {
+      readme,
+      repo: {
+        name: repo.name,
+        description: repo.description,
+        language: repo.language,
+        stars: repo.stargazers_count,
+        forks: repo.forks_count,
+        html_url: repo.html_url,
+        topics: repo.topics || [],
+        updated_at: repo.updated_at,
+      },
+    };
   }
 );
 
-
-
 const ProjectDetailsSlice = createSlice({
   name: "projectDetails",
-  initialState, //state
+  initialState,
   reducers: {
     setProjectName: (state, action) => {
-      console.log(action.payload);
       state.projectName = action.payload;
     },
   },
 });
-//actionlari yayinla
-export const { setProjectName } = ProjectDetailsSlice.actions;
 
-//slice yayinla
+export const { setProjectName } = ProjectDetailsSlice.actions;
 export default ProjectDetailsSlice.reducer;
